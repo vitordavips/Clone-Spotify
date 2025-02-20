bde5ea388a3499d070fcde3ad769e0937efcf797
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2025-02-20 15:24:06 -0400

    novas configurações

--- front-end/src/components/Player/Player.jsx
+++ front-end/src/components/Player/Player.jsx
@@ -7,6 +7,7 @@ import {
   faForwardStep,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+<<<<<<< HEAD
 import { useRef, useEffect } from "react";
 
 const formatTime = (timeInSeconds) => {
@@ -69,6 +70,53 @@ const Player = ({
 
   // setIsPlaying(false)
 
+=======
+import { useEffect, useRef, useState } from 'react';
+
+const formatTime = (timeInSeconds) => {
+  const minutos = Math.floor(timeInSeconds / 60).toString().padStart(2, '0')
+  const seconds = Math.floor(timeInSeconds - minutos * 60).toString().padStart(2, "0")
+
+  return `${minutos}:${seconds}`
+};
+
+const timeInSeconds = (timeString) => {
+  const splitArray = timeString.spit(":")
+  const minutos = Number(splitArray[0])
+  const seconds = Number(splitArray[1])
+
+  return seconds + minutos * 60
+};
+
+const Player = ({durtion, randomIdFromArtist, randomId2FromArtists }) => {
+  const audioPlayer = useRef();
+  const progressBar = useRef();
+  const [isPlaying, setPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(formatTime(0))
+  const durationInSeconds = timeInSeconds(duration)
+
+  const playPause = () => {
+    isPlaying ? audioPlayer.current.pause() : audioPlayer.current.play();
+
+    setPlaying(!isPlaying)
+  }
+
+  useEffect(() => {
+      const intervalId = setInterval(() => { 
+          if(isPlaying)
+              setCurrentTime(formatTime(audioPlayer.current.currentTime))
+
+          progressBar.current.style.setProperty(
+              "--_progress",
+              (audioPlayer.current.currentTime / durationInSeconds) *  100 + "%"
+          )
+      }, 1000)
+
+      return () => clearInterval(intervalId)
+  }, [isPlaying])
+
+  
+>>>>>>> b634bde1a6682a4746d526380bee6330142d0020
   return (
     <div className="player">
       <div className="player__controllers">
